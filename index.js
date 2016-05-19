b5e3fd87c22d22ae4d4ef6aa294a3e5f8397d54c
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2016-05-20 00:05:05 +0200

    polygonEdgeArray and intersectionList fully filled

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ var isects = require('2d-polygon-self-intersections');
 
 // TODO: check default index.
 // TODO: can we work with indeces for line segments?
+// TODO: can constructors such as vertex disapear?
+// NOTE: does the self-intersecting input polygon have a winding number? check paper + if not, add to input requirements in README
+// TODO: add 'expose' to indicate main function. help function (including prototype) can come afterwards
+
 
 // constructors
 var Vertex = function (coord) {
@@ -40,6 +44,7 @@ var nVertex = function (v, param, index, l, incomming) { // (polygon- or interse
   this.incomming = incomming; // index of incomming polygon edge
 };
 // NOTE: added incomming edge
+// TODO: rename, and reorder? ("this is know al 'l' in paper")
 
 var Intersection = function (v, origin1, origin2, index1, index2, winding) { // pair of pseudo-vertices. There is one per self-intersection. The order of 1 and 2 is not important but should be invariant.
   this.v = v; // the vetrex of this pair of pseudo-vertex - Vertex
@@ -58,6 +63,7 @@ module.exports = function(feature) {
   var geom = feature.geometry;
   if (geom.coordinates.length>1) throw new Error("A Polygon input without interior LinearRing's is required");
   var coord = geom.coordinates[0]; // From here on work with exterior LinearRing
+  // TODO: adapt to take just array and not geojson as input. Also adapt output. Define if you want the polygon to be closed or not (see length-1)
 
   // compute self-intersection points
   var isectsData = [];
@@ -82,49 +88,96 @@ module.exports = function(feature) {
     // 1) Add polygon vertices to polygonEdgeArray (as nVertex) and to intersectionList (as Intersection)
     for (var i = 0; i < coord.length-1; i++) {
     	var currentVertex = new Vertex(coord[i]);
-      /*
-      var currentNVertex = new nVertex(currentVertex, 0, undefined, undefined, i); // index is undefined yet
-      // Put currentNVertex as next in polygonEdgeArray
-      polygonEdgeArray.push(currentNVertex);
-      */
       polygonEdgeArray.push([]);
       var currentIntersection = new Intersection(currentVertex, i, undefined, undefined, -1, undefined); // index1 and winding are undefined yet, and origin2 is -1 for polygon-pseudo-vertex
-      // Put currentIntersection as next in intersectionList
+      // TODO: use -1 or undefined? origin2 is also left undefined...
       intersectionList.push(currentIntersection);
     };
     // 2) Add self-intersection vertices to polygonEdgeArray (as nVertex) and (if unique) to intersectionList (as Intersection)
     for (var i = 0; i < isectsData.length; i++) {
     	var currentIsect = isectsData[i];
       var currentVertex = new Vertex(currentIsect[0]);
-      var currentNVertex = new nVertex(currentVertex, currentIsect[8], undefined, currentIsect[4], currentIsect[1]);
-      // Put currentNVertex in polygonEdgeArray in the list of the after nVertex with same l and closest, smaller t
-      polygonEdgeArray[currentIsect[1]].push(currentNVertex);
-      // Put currentIntersection as next in intersectionList
+      var currentNVertex1 = new nVertex(currentVertex, currentIsect[8], undefined, currentIsect[4], currentIsect[1]);
+      //var currentNVertex2 = new nVertex(currentVertex, currentIsect[9], undefined, currentIsect[1], currentIsect[4]);
+      polygonEdgeArray[currentIsect[1]].push(currentNVertex1);
+      //polygonEdgeArray[currentIsect[4]].push(currentNVertex2);
       if (currentIsect[7]){
         var currentIntersection = new Intersection(currentVertex, currentIsect[1], currentIsect[4], undefined, undefined, undefined); // index1 and winding are undefined yet, and origin2 is -1 for polygon-pseudo-vertex
         intersectionList.push(currentIntersection);
       };
     };
-    for (var i = 0; i < coord.length-1; i++) {
+    // Sort Arrays of polygonEdgeArray by param using compareFunction
+    for (var i = 0; i < polygonEdgeArray.length; i++) {
       polygonEdgeArray[i].sort(function(a, b){
-      var keyA = a.param,
-          keyB = b.param;
-      // Compare the 2 dates
-      if(keyA < keyB) return -1;
-      if(keyA > keyB) return 1;
+      if(a.param < b.param) return -1;
+      if(a.param > b.param) return 1;
       return 0;
       });
     };
     //console.log(JSON.stringify(polygonEdgeArray));
     //console.log(JSON.stringify(intersectionList));
 
-    var intersectionQueue = []; // List of nVertex
 
-    // Fill indices
+    // NOTE: check if loops over intersectionList can skip fist polygon edges
+    // Fill indices in polygonEdgeArray
+    for (var i = 0; i < polygonEdgeArray.length; i++){
+      for (var j = 0; j < polygonEdgeArray[i].length; j++){
+        if (j == polygonEdgeArray[i].length-1) {
+          polygonEdgeArray[i][j].index = i + 1; // NOTE: 9 will refer to 10, which may not be in coord
+        } else {
+          for (var k = 0; k < intersectionList.length; k++) {
+            if (intersectionList[k].v.coord.equals(polygonEdgeArray[i][j].v.coord)) {
+              polygonEdgeArray[i][j].index = k;
+            };
+          };
+        };
+      };
+    };
+
+    /*
+    console.log(JSON.stringify(polygonEdgeArray[0]));
+    console.log(JSON.stringify(polygonEdgeArray[3]));
+    console.log("---");
+    console.log(polygonEdgeArray[0][0]==polygonEdgeArray[0][1]);
+    */
+
+    //console.log(JSON.stringify(polygonEdgeArray));
+    // NOTE: This could be done more efficiently. Join?, ...
+    for (var i = 0; i < coord.length-1; i++) {
+      //console.log(i);
+      if (polygonEdgeArray[i].length == 0) {
+        intersectionList[i].index1 = i + 1; // NOTE: 9 will refer to 10, which may not be in coord
+      } else {
+        //console.log(JSON.stringify(polygonEdgeArray[i][0]));
+        intersectionList[i].index1 = polygonEdgeArray[i][0].index;
+      }
+    };
+    for (var i = coord.length-1; i < intersectionList.length; i++) {
+      for (var j = 0; j < polygonEdgeArray.length; j++) {
+        for (var k = 0; k < polygonEdgeArray[j].length; k++) {
+          //console.log(JSON.stringify(intersectionList[i])+" and "+JSON.stringify(polygonEdgeArray[j][k]));
+          if (intersectionList[i].v.coord.equals(polygonEdgeArray[j][k].v.coord)) { // This will happen twice
+            //console.log(JSON.stringify(intersectionList[i].origin1)+" and "+JSON.stringify(intersectionList[i].origin2)+" and "+JSON.stringify(polygonEdgeArray[j][k].incomming)+" and "+JSON.stringify(polygonEdgeArray[j][k].l));
+            //console.log(polygonEdgeArray[j][k].index);
+            if (intersectionList[i].origin1 == polygonEdgeArray[j][k].incomming) {
+              intersectionList[i].index1 = polygonEdgeArray[j][k].index;
+            } else {
+              intersectionList[i].index2 = polygonEdgeArray[j][k].index;
+            };
+          };
+        };
+      };
+    };
+
+    //console.log(JSON.stringify(polygonEdgeArray));
+    //console.log(JSON.stringify(intersectionList));
+
     // Start at outer (convex) point and jump though intersectionList and make polygon by storing vertices.
     // When arriving at a pseudo-vertex that is not a polygon-pseudo-vertex, store it.
     // When done, take next nVertex from queue (check if ...)
     // It's 'index' will tell you where to jump next in the intersectionList if you want to make a polygon on the other side
+    var intersectionQueue = []; // List of nVertex
+
 
 
 
@@ -143,3 +196,34 @@ module.exports = function(feature) {
     };
   }
 };
+
+// Function to compare Arrays of numbers, from http://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
